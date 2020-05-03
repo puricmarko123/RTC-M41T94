f625791c0a71a779f25f8866dc18adc076c698c9
Marko Puric <puric.marko123@gmail.com>
2020-05-03 11:34:20 +0200

    Update

--- RTC_M41T94.h
+++ RTC_M41T94.h
@@ -1,6 +1,5 @@
 /*
- * @what: Module for RTC M41T94
- * @why: Real time clock support
+ * @what: Driver for RTC M41T94
  * @who: Marko Puric
  * @when: 09.01.2020
  */
@@ -9,8 +8,6 @@
 #define RTC_M41T94_H
 
 void M41T94_Init(void);
-uint8_t M41T94_Read_Register(uint8_t address);
-void M41T94_Write_Register(uint8_t address, uint8_t data);
 void M41T94_Read_RTC(void);
 
 uint8_t RTC_Get_Seconds(void);
@@ -20,9 +17,5 @@ uint8_t RTC_Get_Weekday(void);
 uint8_t RTC_Get_Day(void);
 uint8_t RTC_Get_Month(void);
 uint8_t RTC_Get_Year(void);
-
-uint8_t bin2bcd(uint8_t bin);
-uint8_t bcd2bin(uint8_t bcd);
-
 
 #endif /* RTC_M41T94_H_ */
